6f43b122f50c9e9b04ec741f4093c77079f08f4e
Lunavole <lunavole.dev@gmail.com>
2023-11-19 18:19:37 +0100

    🔗 Add link to the footer

--- src/lib/components/nav/Footer.svelte
+++ src/lib/components/nav/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="w-full variant-filled-surface p-4 flex gap-4 justify-between">
     <nav class="flex gap-4">
-        <p class="cursor-pointer"><i class="fa-brands fa-github"></i> Github</p>
-        <p class="cursor-pointer"><i class="fa-solid fa-envelopes-bulk"></i> Contact</p>
+        <a href="https://github.com/Lunavole/character-tavern" class="cursor-pointer" target="_blank"><i class="fa-brands fa-github"></i> Github</a>
+        <p class="cursor-pointer"><i class="fa-brands fa-discord"></i> Contact me on Discord : _salamy</p>
     </nav>
     <p class="font-thin">Data from <a href="https://chub.ai" target="_blank" class="underline">Chub.ai</a> <i class="fa-solid fa-square-arrow-up-right"></i></p>
 </footer>
