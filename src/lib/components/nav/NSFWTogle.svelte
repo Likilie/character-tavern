da2e12ddd666c11436047e8f9f1898c73ca8ca77
Lunavole <lunavole.dev@gmail.com>
2023-11-22 00:11:45 +0100

    🪲 BugFix: Search not working if you disable NSFW and NSFW_Only was true

--- src/lib/components/nav/NSFWTogle.svelte
+++ src/lib/components/nav/NSFWTogle.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
     import { SearchFilterStore } from "$lib/stores/SearchFilter";
-
-
     import { SlideToggle } from "@skeletonlabs/skeleton";
 
+    function VerifyTags() {
+        if ($SearchFilterStore.nsfw_only === true && $SearchFilterStore.nsfw === false) {
+            console.warn("NSFW Only is true, but NSFW is false. Setting NSFW Only to false.");
+            $SearchFilterStore.nsfw_only = false;
+        }
+    }
+
 </script>
-<SlideToggle name="NSFWToggle" active="bg-primary-500" bind:checked={$SearchFilterStore.nsfw}><span class="font-bold">NSFW</span></SlideToggle>
+<SlideToggle name="NSFWToggle" active="bg-primary-500" bind:checked={$SearchFilterStore.nsfw} on:change={VerifyTags}><span class="font-bold">NSFW</span></SlideToggle>
